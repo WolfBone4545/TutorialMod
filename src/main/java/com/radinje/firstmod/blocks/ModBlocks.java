66f51423cc23885d97c9ffd916ee59d8cd1e8063
WolfBone4545 <65337592+WolfBone4545@users.noreply.github.com>
2020-06-16 14:15:07 +0200

    Add files via upload
    Finally uploaded most of the files to GitHub

---
+++ src/main/java/com/radinje/firstmod/blocks/ModBlocks.java
@@ -0,0 +1,8 @@
+package com.radinje.firstmod.blocks;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+public class ModBlocks {
+    @ObjectHolder("firstmod:firstblock")
+    public static FirstBlock FIRSTBLOCK;
+}
---
+++ src/main/java/com/radinje/firstmod/setup/IProxy.java
@@ -0,0 +1,8 @@
+package com.radinje.firstmod.setup;
+
+import net.minecraft.world.World;
+
+public interface IProxy {
+
+    World getClientWorld();
+}
